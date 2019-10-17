d6132f3260ebf28d7be4a377521a228b6983600d
fatjosephina <55261891+fatjosephina@users.noreply.github.com>
2019-10-17 18:08:09 -0500

    Added ramp support and fixed bug
    Added right moving ramp support and attached ramp to moving ramp supports and fixed bug where ball pushed ramp down

--- Gross Warm Beer Unity Project/Assets/Scripts/RampSupport.cs
+++ Gross Warm Beer Unity Project/Assets/Scripts/RampSupport.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class RampSupport : MonoBehaviour
 {
-    public float moveSpeed = 0.5f;
+    public float moveSpeed = 1f;
+    public string inputAxis;
 
     private float verticalInput;
     private Rigidbody2D rigidbody;
@@ -14,12 +15,16 @@ public class RampSupport : MonoBehaviour
         rigidbody = GetComponent<Rigidbody2D>();
     }
 
+    private void Update()
+    {
+        verticalInput = Input.GetAxis(inputAxis);
+    }
+
     // Use fixed update for physics code, because 
     // we need to be careful about how often we call expensive, 
     // hardware intenstive physics stuff
     private void FixedUpdate()
     {
-        verticalInput = Input.GetAxis("Vertical");
         rigidbody.velocity = new Vector2(0, verticalInput * moveSpeed);
     }
 }
