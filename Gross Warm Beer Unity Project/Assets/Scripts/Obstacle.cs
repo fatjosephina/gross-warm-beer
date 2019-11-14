e419f50c625655632e2290ce92fd33af1c9a46f6
fatjosephina <55261891+fatjosephina@users.noreply.github.com>
2019-11-14 17:09:13 -0600

    Added obstacle functionality

---
+++ Gross Warm Beer Unity Project/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public GameObject gameOverText;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Teeeeech!");
+        if (collision.tag == "Player")
+        {
+            gameOverText.SetActive(true);
+            audioSource.Play();
+            Debug.Log("Touched obstacle >:(");
+        }
+    }
+}
